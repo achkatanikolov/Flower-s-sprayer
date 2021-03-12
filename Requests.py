d0050654e600f347f53f3ce522a1eb0ceaa3f764
EmilianaAP <ema.andr33va@gmail.com>
2021-03-12 17:30:10 +0200

    Requests for weather 2.0

--- Requests.py
+++ Requests.py
@@ -1,4 +1,7 @@
 import pyowm
+from pyowm.utils import config
+from pyowm.utils import timestamps
+
 
 owm = pyowm.OWM('42a62878cec9bcfc979975d7ef7f057c')
 
@@ -7,5 +10,7 @@ mgr = owm.weather_manager()
 observation = mgr.weather_at_place('Sofia, BG')
 
 w = observation.weather
+m = w.temperature('celsius')
 
 print(w)
+print(m)
